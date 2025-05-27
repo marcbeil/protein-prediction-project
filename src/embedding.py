ad9e9212c838ba5768f362161f000f555849aaf3
marcbeil <marcbeil2001@gmail.com>
2025-05-27 17:45:24 +0200

    create embedding script

--- src/embedding.py
+++ src/embedding.py
@@ -1,6 +1,11 @@
+import argparse
+import os
 import re
 
+import numpy as np
+import pandas as pd
 import torch
+from tqdm import tqdm
 from transformers import T5EncoderModel, T5Tokenizer
 
 
@@ -30,5 +35,71 @@ class ProtT5Embeddings():
         with torch.no_grad():
             embedding_repr = self.model(input_ids=input_ids, attention_mask=attention_mask)
 
-        # embedding = embedding_repr.last_hidden_state[0, :len(sequence)]
         return embedding_repr.last_hidden_state
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate ProtT5 embeddings for protein sequences from a CSV file.",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    input_group = parser.add_argument_group(title='Input',
+                                            description='Input data parameters')
+    input_group.add_argument('-i', '--input-file', type=str, required=True,
+                             help='Path to the input CSV file containing protein sequences and IDs.')
+
+    output_group = parser.add_argument_group(title='Output',
+                                             description='Output parameters')
+    output_group.add_argument('-o', '--output-folder', type=str, required=True,
+                              help='Path to the output folder where embeddings will be saved as .npy files.')
+
+    model_group = parser.add_argument_group(title='Model',
+                                            description='ProtT5 model parameters')
+    model_group.add_argument('-m', '--model-name', default="Rostlab/prot_t5_xl_half_uniref50-enc", type=str,
+                             help='Name or path of the ProtT5 encoder model to use.')
+
+    processing_group = parser.add_argument_group(title='Processing',
+                                                 description='Processing parameters')
+    processing_group.add_argument('-b', '--batch-size', default=16, type=int,
+                                  help='Batch size for processing sequences.')
+    processing_group.add_argument('--overwrite', action='store_true',
+                                  help='Overwrite existing embedding files. If set, existing files will be overwritten.')
+
+    args = parser.parse_args()
+
+    cwd = os.getcwd()
+
+    dataset = pd.read_csv(os.path.join(cwd, args.input_file))
+    dataset['protein_sequence_length'] = dataset['protein_sequence'].str.len()
+    output_dir = os.path.join(cwd, args.output_folder)
+    os.makedirs(output_dir, exist_ok=True)
+    if not args.overwrite:
+        already_embedded = list(map(lambda filename: filename[:-4], os.listdir(output_dir)))
+        original_len = len(dataset)
+        dataset = dataset[~dataset['domain_id'].isin(already_embedded)]
+        print(f"Removed {original_len - len(dataset)} sequences: already embedded")
+        if len(dataset) == 0:
+            print("All sequences already embedded")
+            return
+
+    dataset.sort_values(by='protein_sequence_length', inplace=True)
+
+    protT5Embeddings = ProtT5Embeddings(model_name=args.model_name)
+
+    for i in tqdm(range(0, len(dataset), args.batch_size), desc="Processing batches"):
+        batch_df = dataset[i:i + args.batch_size]
+        prot_sequences = batch_df['protein_sequence'].tolist()
+        domain_ids = batch_df['domain_id'].tolist()
+
+        if prot_sequences:
+            embeddings = protT5Embeddings.embed_sequences(prot_sequences)
+            for j, domain_id in enumerate(domain_ids):
+                prot_sequence = prot_sequences[j]
+                protein_embedding = embeddings[j, :len(prot_sequence)].cpu()
+                protein_embedding_path = os.path.join(output_dir, f"{domain_id}")
+                np.save(protein_embedding_path, protein_embedding)
+
+
+if __name__ == '__main__':
+    main()
